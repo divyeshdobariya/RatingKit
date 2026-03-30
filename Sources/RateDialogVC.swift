4c2dcbb6cdcf7499a490c3e81b26503ccd620960
Divyesh Dobariya <divyeshdobariya2593@gmail.com>
2026-03-30 17:37:03 +0530

    1.0

--- Sources/RateDialogVC.swift
+++ Sources/RateDialogVC.swift
@@ -84,10 +84,12 @@ public class RateDialogVC: UIViewController, TagListViewDelegate {
         btn_notNow.setTitle(maybeLaterTitle, for: .normal)
         
         btn_submit.setTitle(rateNowTitle, for: .normal)
+        
         btn_submit.tintColor = .white
         btn_submit.titleLabel?.font = .boldSystemFont(ofSize: 20.0)
-        
+        btn_submit.layer.cornerRadius = 16.0
         btn_submit.backgroundColor = tintColor
+        
         btn_notNow.tintColor = tintColor
         btn_neverShow.tintColor = tintColor
         
