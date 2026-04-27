70806bb61cba0f60d6f47521aea0556f674da985
Divyesh Dobariya <divyeshdobariya2593@gmail.com>
2026-04-27 17:19:04 +0530

    without ads uploaded

--- Package.swift
+++ Package.swift
@@ -13,23 +13,9 @@ let package = Package(
         ),
     ],
     
-    dependencies: [
-        // ✅ Add Google Mobile Ads SDK
-        .package(
-            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
-            from: "10.0.0"
-        )
-    ],
-    
     targets: [
         .target(
             name: "RatingKit",
-            dependencies: [
-                .product(
-                    name: "GoogleMobileAds",
-                    package: "swift-package-manager-google-mobile-ads"
-                )
-            ],
             resources: [
                 .process("Resources")
             ]
